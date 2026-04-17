96826217825b4995c0954672b163ed03da88038a
Marcelo Martinez <marcelomartinezpavez@gmail.com>
2026-04-17 10:44:46 -0400

    cambios de coors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081", // tu backend Spring Boot
+  baseURL: "http://45.236.129.192:8080", // tu backend Spring Boot
 });
 
 export default api;
